b1c0f5a14e093e942d5e01f1d14f1d1f9f03a905
Senealukard <koc.ezenarslan@protonmail.com>
2025-05-30 22:01:07 +0300

    Small test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  if (argc == 0)
+    std::cout << "X" << std::endl;
+  else {
+    std::cout << argv;
+  }
+  return 0;
+}
