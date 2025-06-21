406087e9dcc550a17d97aea79bcc51a2d58242b4
Senealukard <koc.ezenarslan@protonmail.com>
2025-06-22 00:56:53 +0300

    as

--- src/simple_main.cpp
+++ src/simple_main.cpp
@@ -70,7 +70,7 @@ int main() {
       }
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(500);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 
   return 0;
